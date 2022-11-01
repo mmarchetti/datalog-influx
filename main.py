6f39cf102185f3c2215e16bf29f3a79de75f5a8d
Michael Marchetti <mike@themarchettis.com>
2022-10-31 20:33:56 -0400

    basic version with separate points

---
+++ main.py
@@ -0,0 +1,152 @@
+#!/usr/bin/env python3
+
+import configparser
+import mmap
+import sys
+from datetime import datetime
+
+import influxdb_client
+from influxdb_client.client.write_api import SYNCHRONOUS
+from influxdb_client import Point, WritePrecision
+
+from datalog import DataLogReader
+
+config_filename = "config.ini"
+BATCH_SIZE = 1000
+
+def read_config(filename):
+    config = configparser.ConfigParser()
+    try:
+        config.read(filename)
+        return dict(config["datalog-influx"])
+    except configparser.ParsingError as exc:
+        print("problem parsing config file:", str(exc), file=sys.stderr)
+
+def main():
+    config = read_config(config_filename)
+    bucket = config["bucket"]
+
+    client = influxdb_client.InfluxDBClient.from_config_file(config_filename)
+    write_api = client.write_api(write_options=SYNCHRONOUS)
+    warned_types = set()
+
+    if len(sys.argv) != 2:
+        print("Usage: main.py <file>", file=sys.stderr)
+        sys.exit(1)
+
+    with open(sys.argv[1], "r") as f:
+        mm = mmap.mmap(f.fileno(), 0, access=mmap.ACCESS_READ)
+        reader = DataLogReader(mm)
+        if not reader:
+            print("not a log file", file=sys.stderr)
+            sys.exit(1)
+
+        entries = {}
+        baseTime = None
+        prevTime = None
+        batch = []
+
+        for record in reader:
+            timestamp = record.timestamp / 1000
+            if record.isStart():
+                try:
+                    data = record.getStartData()
+                    print(
+                        f"Start({data.entry}, name='{data.name}', type='{data.type}', metadata='{data.metadata}') [{timestamp}]"
+                    )
+                    if data.entry in entries:
+                        print("...DUPLICATE entry ID, overriding")
+                    entries[data.entry] = data
+                except TypeError as e:
+                    print("Start(INVALID)")
+            elif record.isFinish():
+                try:
+                    entry = record.getFinishEntry()
+                    print(f"Finish({entry}) [{timestamp}]")
+                    if entry not in entries:
+                        print("...ID not found")
+                    else:
+                        del entries[entry]
+                except TypeError as e:
+                    print("Finish(INVALID)")
+            elif record.isSetMetadata():
+                try:
+                    data = record.getSetMetadataData()
+                    print(f"SetMetadata({data.entry}, '{data.metadata}') [{timestamp}]")
+                    if data.entry not in entries:
+                        print("...ID not found")
+                except TypeError as e:
+                    print("SetMetadata(INVALID)")
+            elif record.isControl():
+                print("Unrecognized control record")
+            else:
+                #print(f"Data({record.entry}, size={len(record.data)}) ", end="")
+                entry = entries.get(record.entry, None)
+                if entry is None:
+                    print("<ID not found>")
+                    continue
+                #print(f"<name='{entry.name}', type='{entry.type}'> [{timestamp}]")
+
+                try:
+                    # handle systemTime specially
+                    if entry.name == "systemTime" and entry.type == "int64":
+                        dt = datetime.fromtimestamp(record.getInteger() / 1000000)
+                        baseTime = record.getInteger() / 1000 - timestamp
+                        #print("  {:%Y-%m-%d %H:%M:%S.%f}".format(dt))
+                        continue
+
+                    value = None
+                    
+                    if entry.type == "double":
+                        value = record.getDouble()
+                        #print(f"  {record.getDouble()}")
+                    elif entry.type == "int64":
+                        value = record.getInteger()
+                        #print(f"  {record.getInteger()}")
+                    elif entry.type == "string":
+                        value = record.getString()
+                        #print(f"  '{record.getString()}'")
+                    elif entry.type == "json":
+                        #print(f"  '{record.getString()}'")
+                        pass
+                    elif entry.type == "boolean":
+                        value = record.getBoolean()
+                        #print(f"  {record.getBoolean()}")
+                    elif entry.type == "boolean[]":
+                        arr = record.getBooleanArray()
+                        #print(f"  {arr}")
+                    elif entry.type == "double[]":
+                        arr = record.getDoubleArray()
+                        #print(f"  {arr}")
+                    elif entry.type == "float[]":
+                        arr = record.getFloatArray()
+                        #print(f"  {arr}")
+                    elif entry.type == "int64[]":
+                        arr = record.getIntegerArray()
+                        #print(f"  {arr}")
+                    elif entry.type == "string[]":
+                        arr = record.getStringArray()
+                        #print(f"  {arr}")
+
+                    if value is None:
+                        if entry.type not in warned_types:
+                            print("skipping unsupported type: " + entry.type)
+                            warned_types.add(entry.type)
+                    elif baseTime is not None:
+                        t = int(baseTime + timestamp)
+
+                        if prevTime is not None and t != prevTime and batch:
+                            write_api.write(bucket=bucket, record=batch)
+                            batch = []
+                            prevTime = t
+
+                        p = Point(entry.name).time(t, write_precision=WritePrecision.MS).field("_value", value)
+                        batch.append(p)
+
+                except TypeError as e:
+                    print("  invalid")
+        if batch:
+            write_api.write(bucket=bucket, record=batch)
+            batch = []
+
+main()
